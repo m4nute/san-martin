e92ecb30d1d5d5f6ebdf33de0cdb6db64462493d
m4nutee <mgudinocontacto@gmail.com>
2022-09-19 12:24:55 -0300

    nv error

--- src/components/Nabvar.js
+++ src/components/Nabvar.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react'
 import { Link } from 'react-router-dom'
-import '../styles/Navbar.css'
+import '../styles/Navbar.scss'
 import Aos from 'aos'
 import 'aos/dist/aos.css'
 
